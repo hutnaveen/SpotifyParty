14e4f3cba33e9fe4343e16a5a22881e5af2019ab
hutnaveen <36118508+hutnaveen@users.noreply.github.com>
2020-06-18 20:56:57 -0700

    added descriptions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SpotifyParty-master
  
+![Image of usage](https://github.com/hutnaveen/SpotifyParty/blob/master/src/SpotifyBG.jpg?raw=true)
+ 
--- src/main/SpotifyParty.java
+++ src/main/SpotifyParty.java
@@ -3,6 +3,9 @@ package main;
 import gui.SpotifyPartyFrame;
 import gui.SpotifyPartyPanel;
 
+/**
+ * @author Naveen Govindaraju, Dhaunsh Ramkumar
+ */
 public class SpotifyParty {
     public static void main(String[] args) {
         System.setProperty("apple.awt.UIElement", "true");
