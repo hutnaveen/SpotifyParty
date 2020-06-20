4608f23c9a398aa5d90a039db4efc58024c93a32
Dhanush03-Dhanush <67134476+Dhanush03-Dhanush@users.noreply.github.com>
2020-06-19 17:09:39 -0700

    abc

--- src/chatGUI/JoinPartyPanel.java
+++ src/chatGUI/JoinPartyPanel.java
@@ -17,26 +17,27 @@ public class JoinPartyPanel extends JPanel {
 
         ImageIcon icon = resizeIcon(new ImageIcon(getClass().getResource("/slice4.png")), 200, 70);
         enter = makeButton("", icon);
-        enter.setBounds(100, 100,200, 70);
+        enter.setBounds(250, 325,200, 70);
         this.add(enter);
 
         JLabel text = new JLabel("Join Party", SwingConstants.CENTER);
+        text.setFont(new Font("Proxima Nova", Font.BOLD, 50));
         text.setForeground(Color.WHITE);
-        text.setBounds(100, 100, 100, 100);
+        text.setBounds(150, 145, 400, 100);
         this.add(text);
 
         JTextField code = new RoundJTextField(200);
         code.setForeground(Color.GRAY);
         code.setText("Code");
-        code.setForeground(Color.BLACK);
-        code.setBounds(130, 250, 200, 50);
+        //code.setForeground(Color.BLACK);
+        code.setBounds(355, 250, 200, 50);
         this.add(code);
 
         JTextField name = new RoundJTextField(200);
         name.setForeground(Color.GRAY);
         name.setText("Name");
-        name.setForeground(Color.BLACK);
-        name.setBounds(340, 250, 200, 50);
+        //name.setForeground(Color.BLACK);
+        name.setBounds(145, 250, 200, 50);
         this.add(name);
     }
 
@@ -44,8 +45,8 @@ public class JoinPartyPanel extends JPanel {
         super.paintComponent(g);
         try
         {
-            g.drawImage(ImageIO.read(getClass().getResource("/logo.png")), 10, 10, 40, 40, this);
             g.drawImage(ImageIO.read(getClass().getResource("/SpotifyBG.jpg")), 0, 0, 700, 600, this);
+            g.drawImage(ImageIO.read(getClass().getResource("/logo.png")), 10, 10, 40, 40, this);
         }
         catch (Exception e)
         {
