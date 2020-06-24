596ace205d08ab9a09ac803ccbf438da5c00f71a
Dhanush03-Dhanush <67134476+Dhanush03-Dhanush@users.noreply.github.com>
2020-06-23 17:46:47 -0700

    asdd

--- src/chatGUI/RequestTab.java
+++ src/chatGUI/RequestTab.java
@@ -5,16 +5,32 @@ import spotifyAPI.SpotifyAppleScriptWrapper;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 import java.awt.*;
 
 public class RequestTab extends JPanel {
     public static SpotifyPlayerAPI api = new SpotifyAppleScriptWrapper();
 
     public RequestTab(String uri) {
+        Border border = BorderFactory.createLineBorder(new Color(40, 40, 40), 1);
         this.setSize(430, 80);
         this.setBackground(Color.BLACK);
 
-
+        JTextPane song = new JTextPane();
+        song.setBorder(border);
+        song.setBackground(new Color(40, 40, 40));
+        song.setForeground(Color.WHITE);
+        song.setEditable(false);
+        song.setFont(new Font("Proxima Nova", Font.BOLD, 13));
+        StyledDocument doc = song.getStyledDocument();
+        SimpleAttributeSet center = new SimpleAttributeSet();
+        StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+        doc.setParagraphAttributes(0, doc.getLength(), center, false);
+        song.setBounds(10, 520, 230, 17);
+        this.add(song);
 
     }
 
