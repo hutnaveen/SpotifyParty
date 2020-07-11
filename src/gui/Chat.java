528842cf8742e937186aae964c398072d33e4091
Dhanush03-Dhanush <67134476+Dhanush03-Dhanush@users.noreply.github.com>
2020-07-11 12:50:32 -0700

    This shit works

--- src/gui/Chat.java
+++ src/gui/Chat.java
@@ -4,7 +4,13 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.text.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
+
+
 
 
 public class Chat extends JPanel {
@@ -13,8 +19,14 @@ public class Chat extends JPanel {
     public static JTextPane chat;
     public static ArrayList<RequestTab> requestTabs = new ArrayList<>();
     public String prev = "";
-
+    private static BufferedImage icon;
     public Chat() {
+        try {
+            icon = ImageIO.read(Notification.class.getResource("/images/logo.png"));
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
         this.setLayout(null);
 
         this.setLocation(0, 0);
@@ -54,31 +66,61 @@ public class Chat extends JPanel {
             }
         }
     }
-
+    boolean you;
     public void addText(String text, String name) {
+        /*
+        if(spfc.getRootPane().isVisible()) {
+            System.out.println("hi");
+            Notification notification = new Notification(icon, "SpotifyParty", name, text, 5000);
+            notification.addActivationListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    spfc.toFront();
+                    spfc.requestFocus();
+                }
+            });
+            notification.send();
+        }
+
+         */
         StyledDocument doc = chat.getStyledDocument();
         SimpleAttributeSet left = new SimpleAttributeSet();
+        SimpleAttributeSet right = new SimpleAttributeSet();
         StyleConstants.setAlignment(left, StyleConstants.ALIGN_LEFT);
-        doc.setParagraphAttributes(0, doc.getLength(), left, false);
+        StyleConstants.setAlignment(right, StyleConstants.ALIGN_RIGHT);
+        // doc.setParagraphAttributes(0, doc.getLength(), left, false);
         chat.setFont(new Font("CircularSpUIv3T-Bold", Font.PLAIN, 20));
         Style style = chat.addStyle("I'm a Style", null);
-
         if(!prev.equals(name)) {
             if(name.equals(SpotifyPartyPanelChat.FriendName)) {
+                you = true;
                 StyleConstants.setForeground(style, Color.GREEN);
             } else {
+                you = false;
                 StyleConstants.setForeground(style, Color.GRAY);
             }
             StyleConstants.setFontSize(style, 20);
 
-            try { doc.insertString(doc.getLength(), "\n" + name + "\n",style); }
+            try {
+                doc.insertString(doc.getLength(), "\n" + name + "\n",style);
+                if(you)
+                    doc.setParagraphAttributes(doc.getLength() - ("\n" + name + "\n").length(), doc.getLength(), right, false);
+                else
+                    doc.setParagraphAttributes(doc.getLength() - ("\n" + name + "\n").length(), doc.getLength(), left, false);
+            }
             catch (BadLocationException e){}
         }
 
         StyleConstants.setForeground(style, Color.WHITE);
         StyleConstants.setFontSize(style, 15);
 
-        try { doc.insertString(doc.getLength(),  text + "\n",style); }
+        try {
+            doc.insertString(doc.getLength(),  text + "\n",style);
+            if(you)
+                doc.setParagraphAttributes(doc.getLength() - (text + "\n").length(), doc.getLength(), right, false);
+            else
+                doc.setParagraphAttributes(doc.getLength() - (text + "\n").length(), doc.getLength(), left, false);
+        }
         catch (BadLocationException e){}
 
         ChatPanel.chatViewPort.setViewPosition(new Point(0, Integer.MAX_VALUE/4));
