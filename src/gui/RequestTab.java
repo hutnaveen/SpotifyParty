e8b456b2c3fbfdcfa82947bed31198aa60dd9244
Dhanush03-Dhanush <67134476+Dhanush03-Dhanush@users.noreply.github.com>
2020-07-13 17:22:09 -0700

    network fixes

--- src/gui/RequestTab.java
+++ src/gui/RequestTab.java
@@ -32,78 +32,75 @@ public class RequestTab extends JPanel {
 
     public RequestTab(String link, String str) {
         info = SpotifyUtils.getTrack(link);
-        url = link;
-        name = str;
-        this.setLayout(null);
-        this.setBorder(new EmptyBorder(0, 0, 0, 0));
-        this.setOpaque(false);
-        this.setSize(430, 110);
-        this.setBackground(new Color(40, 40, 40));
+            url = link;
+            name = str;
+            this.setLayout(null);
+            this.setBorder(new EmptyBorder(0, 0, 0, 0));
+            this.setOpaque(false);
+            this.setSize(430, 110);
+            this.setBackground(new Color(40, 40, 40));
 
-        JLabel nameLabel = new JLabel(name);
-        nameLabel.setBounds(0, 9, 150, 18);
-        nameLabel.setFont(new Font("CircularSpUIv3T-Bold", Font.PLAIN, 17));
-        nameLabel.setOpaque(false);
-        nameLabel.setForeground(Color.WHITE);
-        this.add(nameLabel);
+            JLabel nameLabel = new JLabel(name);
+            nameLabel.setBounds(0, 9, 150, 18);
+            nameLabel.setFont(new Font("CircularSpUIv3T-Bold", Font.PLAIN, 17));
+            nameLabel.setOpaque(false);
+            nameLabel.setForeground(Color.WHITE);
+            this.add(nameLabel);
 
-        PopupMenu menu = new PopupMenu();
-        if(!SpotifyPartyPanelChat.host) {
-            MenuItem like = new MenuItem("Vote");
-            menu.add(like);
-        }
-
-        if(SpotifyPartyPanelChat.host) {
-            MenuItem play = new MenuItem("Play");
-            if(info == null)
-             info = SpotifyUtils.search(link).get(0);
-            System.out.println(info.getUri());
-            play.addActionListener(e -> api.playTrack(info.getUri()));
-            menu.add(play);
-
-            MenuItem delete = new MenuItem("Delete");
-            delete.addActionListener(e -> {
-                    Requests.redraw(url);
-            });
-            menu.add(delete);
-        }
-
-        ImageIcon playIcon = resizeIcon(new ImageIcon(getClass().getResource("/images/3dots.png")), 23, 6);
-        JLabel opt = new JLabel(playIcon);
-        this.add(opt);
-        this.add(menu);
-        opt.setBounds(380, 67, 30, 6);
-        opt.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                menu.show(e.getComponent(), e.getX(), e.getY());
+            PopupMenu menu = new PopupMenu();
+            if (!SpotifyPartyPanelChat.host) {
+                MenuItem like = new MenuItem("Vote");
+                menu.add(like);
             }
-        });
 
-        song = new JTextPane();
-        song.setFocusable(false);
-        song.setOpaque(false);
-        song.setText(info.getName());
-        song.setBorder(new EmptyBorder(0,0,0,0));
-        song.setForeground(Color.WHITE);
-        song.setEditable(false);
-        song.setFont(new Font("CircularSpUIv3T-Bold", Font.PLAIN, 17));
-        animate(song);
-        StyledDocument doc = song.getStyledDocument();
-        SimpleAttributeSet center = new SimpleAttributeSet();
-        StyleConstants.setAlignment(center, StyleConstants.ALIGN_LEFT);
-        doc.setParagraphAttributes(0, doc.getLength(), center, false);
-        song.setBounds(80, 45, 275, 25);
-        this.add(song);
+            if (SpotifyPartyPanelChat.host) {
+                MenuItem play = new MenuItem("Play");
+                System.out.println(info.getUri());
+                play.addActionListener(e -> api.playTrack(info.getUri()));
+                menu.add(play);
 
-        artist = new JTextPane();
-        artist.setFocusable(false);
-        artist.setOpaque(false);
-        StringBuilder artists = new StringBuilder();
-        for(Artist art: info.getArtists())
-        {
-            artists.append(art.getName() + ", ");
-        }
+                MenuItem delete = new MenuItem("Delete");
+                delete.addActionListener(e -> {
+                    Requests.redraw(url);
+                });
+                menu.add(delete);
+            }
+
+            ImageIcon playIcon = resizeIcon(new ImageIcon(getClass().getResource("/images/3dots.png")), 23, 6);
+            JLabel opt = new JLabel(playIcon);
+            this.add(opt);
+            this.add(menu);
+            opt.setBounds(380, 67, 30, 6);
+            opt.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    menu.show(e.getComponent(), e.getX(), e.getY());
+                }
+            });
+
+            song = new JTextPane();
+            song.setFocusable(false);
+            song.setOpaque(false);
+            song.setText(info.getName());
+            song.setBorder(new EmptyBorder(0, 0, 0, 0));
+            song.setForeground(Color.WHITE);
+            song.setEditable(false);
+            song.setFont(new Font("CircularSpUIv3T-Bold", Font.PLAIN, 17));
+            animate(song);
+            StyledDocument doc = song.getStyledDocument();
+            SimpleAttributeSet center = new SimpleAttributeSet();
+            StyleConstants.setAlignment(center, StyleConstants.ALIGN_LEFT);
+            doc.setParagraphAttributes(0, doc.getLength(), center, false);
+            song.setBounds(80, 45, 275, 25);
+            this.add(song);
+
+            artist = new JTextPane();
+            artist.setFocusable(false);
+            artist.setOpaque(false);
+            StringBuilder artists = new StringBuilder();
+            for (Artist art : info.getArtists()) {
+                artists.append(art.getName() + ", ");
+            }
         /*
         artists.replace(artists.length()-2, artists.length(), "");
         if(artists.length() > 26) {
@@ -112,39 +109,42 @@ public class RequestTab extends JPanel {
         }
 
          */
-        artist.setText(artists.toString());
-        artist.setBorder(new EmptyBorder(0,0,0,0));
-        artist.setForeground(Color.GRAY);
-        artist.setEditable(false);
-        artist.setFont(new Font("CircularSpUIv3T-Bold", Font.PLAIN, 14));
-        animate(artist);
-        StyledDocument doc3 = artist.getStyledDocument();
-        SimpleAttributeSet center3 = new SimpleAttributeSet();
-        StyleConstants.setAlignment(center3, StyleConstants.ALIGN_LEFT);
-        doc3.setParagraphAttributes(0, doc3.getLength(), center3, false);
-        artist.setBounds(80, 69, 275, 25);
-        this.add(artist);
+            artist.setText(artists.toString());
+            artist.setBorder(new EmptyBorder(0, 0, 0, 0));
+            artist.setForeground(Color.GRAY);
+            artist.setEditable(false);
+            artist.setFont(new Font("CircularSpUIv3T-Bold", Font.PLAIN, 14));
+            animate(artist);
+            StyledDocument doc3 = artist.getStyledDocument();
+            SimpleAttributeSet center3 = new SimpleAttributeSet();
+            StyleConstants.setAlignment(center3, StyleConstants.ALIGN_LEFT);
+            doc3.setParagraphAttributes(0, doc3.getLength(), center3, false);
+            artist.setBounds(80, 69, 275, 25);
+            this.add(artist);
 
-        JTextPane inv = new JTextPane();
-        inv.setText("");
-        inv.setOpaque(false);
-        inv.setBorder(new EmptyBorder(0,0,0,0));
-        inv.setBounds(10, 39, 60, 60);
-        inv.setEditable(false);
-        inv.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                super.mousePressed(e);
-                api.playTrack(info.getUri());
-            }
-        });
-        this.add(inv);
+            JTextPane inv = new JTextPane();
+            inv.setText("");
+            inv.setOpaque(false);
+            inv.setBorder(new EmptyBorder(0, 0, 0, 0));
+            inv.setBounds(10, 39, 60, 60);
+            inv.setEditable(false);
+            inv.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    super.mousePressed(e);
+                    if (SpotifyPartyPanelChat.host) {
+                        api.playTrack(info.getUri());
+                    }
+                }
+            });
+            this.add(inv);
 
-        System.out.println(link + " " + str);
-        Requests.backText.setFont(new Font("CircularSpUIv3T-Bold", Font.PLAIN, 8));
-        Requests.backText.setText(Requests.backText.getText() + "\n\n\n\n\n\n\n\n\n\n");
+            System.out.println(link + " " + str);
+            Requests.backText.setFont(new Font("CircularSpUIv3T-Bold", Font.PLAIN, 8));
+            Requests.backText.setText(Requests.backText.getText() + "\n\n\n\n\n\n\n\n\n\n");
 
-        animate(this);
+            animate(this);
+
     }
 
     public void animate(JComponent obj) {
