24721e714ac7117c693a73015ceb46d91b99f8ee
Naveen Govind <naveengovind75@gmail.com>
2020-07-18 17:49:31 -0700

    added update checking

--- src/main/SpotifyParty.java
+++ src/main/SpotifyParty.java
@@ -1,13 +1,22 @@
 package main;
 
 import gui.ChatPanel;
+import gui.Notification;
 import gui.SpotifyPartyPanelChat;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import utils.OSXUtils;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.net.URL;
+import java.sql.SQLOutput;
+
+import static gui.GUIUtilsChat.resizeIcon;
 
 /**
  * @author Naveen Govindaraju, Dhaunsh Ramkumar
@@ -15,6 +24,7 @@ import java.io.PrintStream;
 public class SpotifyParty {
     public static ChatPanel chatPanel = null;
     public static boolean darkMode = true;
+    public static final String VERSION = "v0.2-alpha";
     public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
         System.setProperty("apple.awt.UIElement", "true");
         System.setErr(new PrintStream(new OutputStream() {
@@ -23,6 +33,7 @@ public class SpotifyParty {
 
             }
         }));
+        checkForUpdate();
         chatPanel = new ChatPanel();
             UIManager.setLookAndFeel("org.violetlib.aqua.AquaLookAndFeel");
         try {
@@ -34,4 +45,38 @@ public class SpotifyParty {
         }
         SpotifyPartyPanelChat panel = new SpotifyPartyPanelChat();
     }
+    private static void checkForUpdate()
+    {
+        try {
+            Document doc = Jsoup.connect("https://github.com/naveengovind/SpotifyParty/releases").get();
+            String txt = (doc.getElementsByClass("release-entry").get(0).text());
+           int a =  txt.indexOf(' ') + 1;
+           String newVersion = txt.substring(a,txt.indexOf(" ", a));
+            if(!newVersion.contains(VERSION))
+            {
+                Object[] options = { "UPDATE", "CANCEL" };
+                JFrame frame = new JFrame();
+                frame.setAlwaysOnTop(true);
+                int selectedValue = JOptionPane.showOptionDialog(frame, "Would you like to update SpotifyParty?", "Update Available",
+                        JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+                        resizeIcon(new ImageIcon(ChatPanel.class.getResource("/images/logo.png")), 50, 50), options, options[0]);
+                if(selectedValue == 0)
+                {
+                    String urlStr = "https://github.com/naveengovind/SpotifyParty/releases/download/"+newVersion.trim()+"/SpotfiyParty.dmg";
+                    URL url = new URL(urlStr);
+                    Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+                    if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+                        try {
+                            desktop.browse(url.toURI());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
