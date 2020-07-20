fa8c4944c504fbaab622461b51b45295170561e1
Naveen Govind <naveengovind75@gmail.com>
2020-07-20 04:16:50 -0700

    multi language support for lyrics

--- src/fonts/Test.java
+++ src/fonts/Test.java
@@ -3,18 +3,13 @@ package fonts;
 import gui.Notification;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 public class Test {
     public static void main(String[] args) {
-          BufferedImage icon = null;
-            try {
-                icon = ImageIO.read(Notification.class.getResource("/images/logo.png"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-         Notification notif1  = new Notification(icon, "SpotifyParty", "Banakar", "im excited to drive");
-         notif1.send();
+
     }
 }
--- src/time/Test.java
+++
@@ -1,7 +0,0 @@
-package time;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println(TimeUtils.getAppleTime() + " " + TimeUtils.getAppleTime());
-    }
-}
