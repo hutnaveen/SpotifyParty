67978ccaccbd9afe70892f5bb86e86bf42b4727e
Naveen Govind <naveengovind75@gmail.com>
2020-07-22 03:29:04 -0700

    b

--- src/server/TCPServer.java
+++ src/server/TCPServer.java
@@ -177,7 +177,7 @@ public class TCPServer
                    // e.printStackTrace();
                 }
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(2000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
