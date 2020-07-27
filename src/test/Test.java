8114a9f3386d51c754fb05d26741e15f1d14574a
Naveen Govind <naveengovind75@gmail.com>
2020-07-27 05:31:44 -0700

    moved jars

---
+++ src/test/Test.java
@@ -0,0 +1,39 @@
+import coroutines.KThreadRepKt;
+import spotifyAPI.OSXSpotifyAPI;
+import spotifyAPI.SpotifyWebAPI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class Test {
+    static int i = 0;
+    public static void main(String[] args) {
+        SpotifyWebAPI api = new OSXSpotifyAPI();
+        try {
+            System.setOut(new PrintStream(new File("/Users/naveen/Documents/Idea/SpotifyParty/src/dumpData/data.txt")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        long start = System.currentTimeMillis();
+        System.out.println(start);
+       for(int a = 0; a <= 5; a++)
+       {
+           KThreadRepKt.startCor(() -> {
+               try {
+                   api.getPlayerData();
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+               i++;
+               System.out.println(i + " calls in " + (System.currentTimeMillis() - start)/1000 + " secconds");
+           });
+           System.out.println(a + " Coroutines");
+       }
+
+    }
+}
+
