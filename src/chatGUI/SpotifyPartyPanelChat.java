02c5dcc951c6580392a402333d37625bf497d515
Naveen Govind <naveengovind75@gmail.com>
2020-07-29 06:35:56 -0700

    z

--- src/chatGUI/SpotifyPartyPanelChat.java
+++ src/chatGUI/SpotifyPartyPanelChat.java
@@ -2,6 +2,7 @@
 package chatGUI;
 
 import client.SketchClient;
+import coroutines.KThreadRepKt;
 import gui.JoinPartyPanel;
 import server.SketchServer;
 import utils.NetworkUtils;
@@ -15,6 +16,7 @@ import static chatGUI.SpotifyPartyFrameChat.menu;
 import static chatGUI.SpotifyPartyFrameChat.quit;
 import static main.SpotifyParty.chatPanel;
 import static main.SpotifyParty.api;
+import static main.SpotifyParty.defFont;
 import static utils.GUIUtils.resizeIcon;
 
 public class SpotifyPartyPanelChat extends JPanel implements ActionListener {
@@ -71,12 +73,30 @@ public class SpotifyPartyPanelChat extends JPanel implements ActionListener {
          */
         else if(e.getActionCommand().equals("hostPublic")) {
             Object[] options = { "OK"};
-            JFrame frame = new JFrame();
-            frame.setAlwaysOnTop(true);
-            JOptionPane.showOptionDialog(frame, "Your party will start shortly!", "Spotify Party!",
+            /*JOptionPane.showOptionDialog(frame, "Your party will start shortly!", "Spotify Party!",
                     JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
                     resizeIcon(new ImageIcon(ChatPanel.class.getResource("/images/logo.png")), 50, 50), options, options[0]);
-
+*/
+            JFrame frame = new JFrame("SpotifyParty");
+            KThreadRepKt.startCor(new Runnable() {
+                @Override
+                public void run() {
+                    JPanel panel = new JPanel();
+                    Icon imgIcon = new ImageIcon(getClass().getResource("/images/5f21771971927231816530.gif"));
+                    JLabel label = new JLabel();
+                    label.setIcon(imgIcon);
+                    label.setFont(new Font(defFont, Font.PLAIN, 20));
+                    label.setText(" Your party will start shortly!");
+                    panel.setBackground(Color.WHITE);
+                    frame.getContentPane().setBackground(Color.WHITE);
+                    label.setBackground(Color.WHITE);
+                    panel.add(label);
+                    frame.add(panel);
+                    frame.setAlwaysOnTop(true);
+                    frame.setSize(new Dimension(450, 150));
+                    frame.setVisible(true);
+                }
+            });
             FriendName = api.getUserData().getDisplay_name();
             FriendName = FriendName.replace(" ", "-");
             host = true;
@@ -84,12 +104,12 @@ public class SpotifyPartyPanelChat extends JPanel implements ActionListener {
             server = new SketchServer();
             cl.show(this, "chatPanel");
             spfc.setVisible(true);
+            frame.dispose();
             menu.removeAll();
             menu.add(show);
             menu.addSeparator();
             menu.add(quit);
             chatPanel.updateData();
-
         }
         else if (e.getActionCommand().equals("enterGuest")) {
             FriendName = api.getUserData().getDisplay_name();
